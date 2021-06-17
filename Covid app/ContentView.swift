c6ae7d037d2e6cb8120bd86a84f0cb03bbcc9749
Pierre Ferry <pierre.f.ferry@gmail.com>
2021-06-17 15:34:00 +0200

    Initial Commit

---
+++ Covid app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Covid app
+//
+//  Created by Pierre Ferry on 6/17/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Covid app/Covid_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Covid_appApp.swift
+//  Covid app
+//
+//  Created by Pierre Ferry on 6/17/21.
+//
+
+import SwiftUI
+
+@main
+struct Covid_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
